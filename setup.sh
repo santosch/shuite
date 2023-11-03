d07fb0e1e8df1ea9ece98f1c0299040f8cc6b1c5
Sebastian Antosch <write@sebastian-antosch.de>
2023-11-03 19:11:07 +0100

    load all setups from plugins

--- setup.sh
+++ setup.sh
@@ -10,3 +10,23 @@ echo -e "${COLOR_NO}"
 
 # shuite setup
 . "$SHUITE_ROOT/setup/shuite.sh"
+
+# load plugins
+for PLUGIN in "${SHUITE_PLUGINS[@]}";
+do
+  PLUGIN_PATH="$SHUITE_ROOT/$PLUGIN"
+  if [ -d $PLUGIN_PATH ]
+  then
+    SETUP_PATH="$PLUGIN_PATH/setup.sh"
+    if [ -f $SETUP_PATH ]
+    then
+      echo -e "    - ${COLOR_GREEN}Loading $PLUGIN${COLOR_NO}..."
+      . "$SETUP_PATH"
+    else
+      echo -e "    - ${COLOR_CYAN}Loading $PLUGIN${COLOR_NO}"
+    fi
+  else
+    echo -e "    x ${COLOR_RED}Error loading plugin \"$PLUGIN\"${COLOR_YELLOW} - Folder $PLUGIN_PATH not found${COLOR_NO}"
+  fi
+done
+
--- variables.sh
+++ variables.sh
@@ -8,3 +8,18 @@ COLOR_YELLOW='\033[1;33m'
 COLOR_GREEN='\033[0;32m'
 COLOR_CYAN='\033[0;36m'
 COLOR_BLUE='\033[0;34m'
+
+SHUITE_DEFAULT_PLUGINS=(
+  "plugins/composer"
+  "plugins/ohmybash"
+  "plugins/ssh"
+  "plugins/sync"
+)
+
+if [[ -z $SHUITE_PLUGINS ]];
+then
+  SHUITE_PLUGINS=( "${SHUITE_DEFAULT_PLUGINS[@]}" )
+else
+  # shellcheck disable=SC2206
+  SHUITE_PLUGINS=(${SHUITE_PLUGINS//,/ })
+fi
